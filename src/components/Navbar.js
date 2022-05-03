a79f5d9fb76e9c577714ee06fc88ce3714b71b58
Justin Souza <justinsouza1029@gmail.com>
2022-05-02 20:00:09 -0700

    Installed react-spring

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './Navbar.css';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Link } from 'react-router-dom';
@@ -8,29 +9,32 @@ function Navbar({ totalItems, setIsOpen }) {
     return <div className="item-count">{totalItems}</div>;
   };
 
-  const mobileSidebar = () => (
-    <div className="dark-out">
-      <div className="mobile-sidebar-wrapper">
-        <Link to="/">
-          <button type="button">Home</button>
-        </Link>
-        <button type="button" onClick={() => setIsOpen(true)}>
-          Contact
-        </button>
-        <button type="button">About</button>
-        <Link to="/cart">
-          <button type="button">
-            <ShoppingCartIcon />
-            {totalItems > 0 && itemCountBadge()}
+  const MobileSidebar = () => {
+    return ReactDOM.createPortal(
+      <div className="dark-out">
+        <div className="mobile-sidebar-wrapper">
+          <Link to="/">
+            <button type="button">Home</button>
+          </Link>
+          <button type="button" onClick={() => setIsOpen(true)}>
+            Contact
           </button>
-        </Link>
-      </div>
-    </div>
-  );
+          <button type="button">About</button>
+          <Link to="/cart">
+            <button type="button">
+              <ShoppingCartIcon />
+              {totalItems > 0 && itemCountBadge()}
+            </button>
+          </Link>
+        </div>
+      </div>,
+      document.getElementById('sidebar-portal')
+    );
+  };
 
   return (
     <div className="navbar">
-      <div className="hamburger-menu">
+      <div type="button" className="hamburger-menu">
         <div></div>
         <div></div>
         <div></div>
